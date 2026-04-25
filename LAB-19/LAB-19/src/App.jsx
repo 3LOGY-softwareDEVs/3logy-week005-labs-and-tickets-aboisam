b363ca0a4426ce2ea132c9570d2ffd7730d85401
Aboi Samson Aboi <aboisam53@gmail.com>
2026-04-25 21:06:38 +0100

    lab-19

---
+++ LAB-19/LAB-19/src/App.jsx
@@ -0,0 +1,18 @@
+import StopWatch from './components/StopWatch';
+import PostList from './components/PostList';
+
+
+function App() {
+
+  <>
+    <div>
+      <h1>useEffect Patterns</h1>
+      <StopWatch />
+      <hr />
+      <PostList />
+    </div>
+
+  </>
+
+}
+export default App;
